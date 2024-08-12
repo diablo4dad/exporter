4db1141e5bc4aecf78c449d11f42aeccdceea8f5
Sam Edge <guitarfella@gmail.com>
2024-08-12 21:34:10 +0930

    Refactor software

---
+++ src/json/collections/essential/software.ts
@@ -0,0 +1,453 @@
+import { Category, CollectionDescriptor } from '../../index.js';
+
+const SOFTWARE: CollectionDescriptor = {
+  name: 'Software Purchase',
+  description: 'Transmogs included with Pre-purchase, Deluxe and Ultimate editions',
+  category: Category.GENERAL,
+  items: [],
+  patches: [],
+  children: [
+    {
+      name: 'Vessel of Hatred',
+      description: 'The Vessel of Hatred expansion',
+      category: Category.GENERAL,
+      items: [
+        [
+          'json\\base\\meta\\Item\\cmp_base01_cat.itm.json',
+        ],
+        [
+          'json\\base\\meta\\Item\\cmp_base01_dog.itm.json',
+        ],
+        [
+          'json\\base\\meta\\Item\\mnt_dlux004_cat.itm.json',
+        ],
+        [
+          'json\\base\\meta\\Item\\mnt_amor207_cat_stor.itm.json',
+        ],
+        [
+          'json\\base\\meta\\Item\\mnt_stor218_trophy.itm.json',
+        ],
+        [
+          'json\\base\\meta\\Item\\cmp_base00_cat.itm.json',
+        ],
+        [
+          'json\\base\\meta\\Item\\trophy_glo002_stor.itm.json',
+        ],
+        [
+          'json\\base\\meta\\TownPortalCosmetic\\portal_glo013_stor.tpc.json',
+        ],
+        [
+          'json\\base\\meta\\Item\\Helm_Cosmetic_Barbarian_217_stor.itm.json',
+        ],
+        [
+          'json\\base\\meta\\Item\\Chest_Cosmetic_Barbarian_217_stor.itm.json',
+        ],
+        [
+          'json\\base\\meta\\Item\\Gloves_Cosmetic_Barbarian_217_stor.itm.json',
+        ],
+        [
+          'json\\base\\meta\\Item\\Pants_Cosmetic_Barbarian_217_stor.itm.json',
+        ],
+        [
+          'json\\base\\meta\\Item\\Boots_Cosmetic_Barbarian_217_stor.itm.json',
+        ],
+        [
+          'json\\base\\meta\\Item\\Helm_Cosmetic_Druid_210_stor.itm.json',
+        ],
+        [
+          'json\\base\\meta\\Item\\Chest_Cosmetic_Druid_210_stor.itm.json',
+        ],
+        [
+          'json\\base\\meta\\Item\\Gloves_Cosmetic_Druid_210_stor.itm.json',
+        ],
+        [
+          'json\\base\\meta\\Item\\Pants_Cosmetic_Druid_210_stor.itm.json',
+        ],
+        [
+          'json\\base\\meta\\Item\\Boots_Cosmetic_Druid_210_stor.itm.json',
+        ],
+        [
+          'json\\base\\meta\\Item\\Helm_Cosmetic_Necro_210_stor.itm.json',
+        ],
+        [
+          'json\\base\\meta\\Item\\Chest_Cosmetic_Necro_210_stor.itm.json',
+        ],
+        [
+          'json\\base\\meta\\Item\\Gloves_Cosmetic_Necro_210_stor.itm.json',
+        ],
+        [
+          'json\\base\\meta\\Item\\Pants_Cosmetic_Necro_210_stor.itm.json',
+        ],
+        [
+          'json\\base\\meta\\Item\\Boots_Cosmetic_Necro_210_stor.itm.json',
+        ],
+        [
+          'json\\base\\meta\\Item\\Helm_Cosmetic_Rogue_214_stor.itm.json',
+        ],
+        [
+          'json\\base\\meta\\Item\\Chest_Cosmetic_Rogue_214_stor.itm.json',
+        ],
+        [
+          'json\\base\\meta\\Item\\Gloves_Cosmetic_Rogue_214_stor.itm.json',
+        ],
+        [
+          'json\\base\\meta\\Item\\Pants_Cosmetic_Rogue_214_stor.itm.json',
+        ],
+        [
+          'json\\base\\meta\\Item\\Boots_Cosmetic_Rogue_214_stor.itm.json',
+        ],
+        [
+          'json\\base\\meta\\Item\\Helm_Cosmetic_Sorcerer_213_stor.itm.json',
+        ],
+        [
+          'json\\base\\meta\\Item\\Chest_Cosmetic_Sorcerer_213_stor.itm.json',
+        ],
+        [
+          'json\\base\\meta\\Item\\Gloves_Cosmetic_Sorcerer_213_stor.itm.json',
+        ],
+        [
+          'json\\base\\meta\\Item\\Pants_Cosmetic_Sorcerer_213_stor.itm.json',
+        ],
+        [
+          'json\\base\\meta\\Item\\Boots_Cosmetic_Sorcerer_213_stor.itm.json',
+        ],
+      ],
+      patches: [
+        {
+          items: [
+            1869929,
+          ],
+          claim: 'Promotional',
+          claimDescription: 'Pre-purchase bonus.',
+          premium: true,
+        },
+        {
+          items: [
+            1943442,
+          ],
+          claim: 'Promotional',
+          claimDescription: 'Included with the Vessel of Hatred Deluxe and Ultimate editions.',
+          premium: true,
+        },
+        {
+          items: [
+            1872528,
+          ],
+          claim: 'Promotional',
+          claimDescription: 'Included with the Vessel of Hatred Deluxe and Ultimate editions.',
+          premium: true,
+        },
+        {
+          items: [
+            1841527,
+          ],
+          claim: 'Promotional',
+          claimDescription: 'Included with the Vessel of Hatred Deluxe and Ultimate editions.',
+          premium: true,
+        },
+        {
+          items: [
+            1872515,
+          ],
+          claim: 'Promotional',
+          claimDescription: 'Included with the Vessel of Hatred Deluxe and Ultimate editions.',
+          premium: true,
+        },
+        {
+          items: [
+            1867813,
+          ],
+          claim: 'Promotional',
+          claimDescription: 'Included with the Vessel of Hatred Ultimate edition.',
+          premium: true,
+        },
+        {
+          items: [
+            1866311,
+          ],
+          claim: 'Promotional',
+          claimDescription: 'Included with the Vessel of Hatred Ultimate edition.',
+          premium: true,
+        },
+        {
+          items: [
+            1866254,
+          ],
+          claim: 'Promotional',
+          claimDescription: 'Included with the Vessel of Hatred Ultimate edition.',
+          premium: true,
+        },
+        {
+          items: [
+            1744607,
+          ],
+          claim: 'Promotional',
+          claimDescription: 'Included with the Vessel of Hatred Ultimate edition.',
+          premium: true,
+        },
+        {
+          items: [
+            1744614,
+          ],
+          claim: 'Promotional',
+          claimDescription: 'Included with the Vessel of Hatred Ultimate edition.',
+          premium: true,
+        },
+        {
+          items: [
+            1744622,
+          ],
+          claim: 'Promotional',
+          claimDescription: 'Included with the Vessel of Hatred Ultimate edition.',
+          premium: true,
+        },
+        {
+          items: [
+            1744629,
+          ],
+          claim: 'Promotional',
+          claimDescription: 'Included with the Vessel of Hatred Ultimate edition.',
+          premium: true,
+        },
+        {
+          items: [
+            1744636,
+          ],
+          claim: 'Promotional',
+          claimDescription: 'Included with the Vessel of Hatred Ultimate edition.',
+          premium: true,
+        },
+        {
+          items: [
+            1815593,
+          ],
+          claim: 'Promotional',
+          claimDescription: 'Included with the Vessel of Hatred Ultimate edition.',
+          premium: true,
+        },
+        {
+          items: [
+            1815599,
+          ],
+          claim: 'Promotional',
+          claimDescription: 'Included with the Vessel of Hatred Ultimate edition.',
+          premium: true,
+        },
+        {
+          items: [
+            1815605,
+          ],
+          claim: 'Promotional',
+          claimDescription: 'Included with the Vessel of Hatred Ultimate edition.',
+          premium: true,
+        },
+        {
+          items: [
+            1815611,
+          ],
+          claim: 'Promotional',
+          claimDescription: 'Included with the Vessel of Hatred Ultimate edition.',
+          premium: true,
+        },
+        {
+          items: [
+            1815617,
+          ],
+          claim: 'Promotional',
+          claimDescription: 'Included with the Vessel of Hatred Ultimate edition.',
+          premium: true,
+        },
+        {
+          items: [
+            1815772,
+          ],
+          claim: 'Promotional',
+          claimDescription: 'Included with the Vessel of Hatred Ultimate edition.',
+          premium: true,
+        },
+        {
+          items: [
+            1815778,
+          ],
+          claim: 'Promotional',
+          claimDescription: 'Included with the Vessel of Hatred Ultimate edition.',
+          premium: true,
+        },
+        {
+          items: [
+            1815784,
+          ],
+          claim: 'Promotional',
+          claimDescription: 'Included with the Vessel of Hatred Ultimate edition.',
+          premium: true,
+        },
+        {
+          items: [
+            1815790,
+          ],
+          claim: 'Promotional',
+          claimDescription: 'Included with the Vessel of Hatred Ultimate edition.',
+          premium: true,
+        },
+        {
+          items: [
+            1815796,
+          ],
+          claim: 'Promotional',
+          claimDescription: 'Included with the Vessel of Hatred Ultimate edition.',
+          premium: true,
+        },
+        {
+          items: [
+            1825193,
+          ],
+          claim: 'Promotional',
+          claimDescription: 'Included with the Vessel of Hatred Ultimate edition.',
+          premium: true,
+        },
+        {
+          items: [
+            1825199,
+          ],
+          claim: 'Promotional',
+          claimDescription: 'Included with the Vessel of Hatred Ultimate edition.',
+          premium: true,
+        },
+        {
+          items: [
+            1825205,
+          ],
+          claim: 'Promotional',
+          claimDescription: 'Included with the Vessel of Hatred Ultimate edition.',
+          premium: true,
+        },
+        {
+          items: [
+            1825211,
+          ],
+          claim: 'Promotional',
+          claimDescription: 'Included with the Vessel of Hatred Ultimate edition.',
+          premium: true,
+        },
+        {
+          items: [
+            1825217,
+          ],
+          claim: 'Promotional',
+          claimDescription: 'Included with the Vessel of Hatred Ultimate edition.',
+          premium: true,
+        },
+        {
+          items: [
+            1825626,
+          ],
+          claim: 'Promotional',
+          claimDescription: 'Included with the Vessel of Hatred Ultimate edition.',
+          premium: true,
+        },
+        {
+          items: [
+            1825622,
+          ],
+          claim: 'Promotional',
+          claimDescription: 'Included with the Vessel of Hatred Ultimate edition.',
+          premium: true,
+        },
+        {
+          items: [
+            1825624,
+          ],
+          claim: 'Promotional',
+          claimDescription: 'Included with the Vessel of Hatred Ultimate edition.',
+          premium: true,
+        },
+        {
+          items: [
+            1825628,
+          ],
+          claim: 'Promotional',
+          claimDescription: 'Included with the Vessel of Hatred Ultimate edition.',
+          premium: true,
+        },
+        {
+          items: [
+            1825620,
+          ],
+          claim: 'Promotional',
+          claimDescription: 'Included with the Vessel of Hatred Ultimate edition.',
+          premium: true,
+        },
+      ],
+      children: [],
+    },
+    {
+      name: 'Base Game',
+      description: 'The original release of Diablo IV',
+      category: Category.GENERAL,
+      items: [
+        [
+          'json\\base\\meta\\Item\\mnt_stor004_horse.itm.json',
+        ],
+        [
+          'json\\base\\meta\\Item\\mnt_amor107_horse.itm.json',
+        ],
+        [
+          'json\\base\\meta\\Item\\mnt_dlux001_horse.itm.json',
+        ],
+        [
+          'json\\base\\meta\\Item\\mnt_amor100_horse_dlux.itm.json',
+        ],
+        [
+          'json\\base\\meta\\Emote\\emote_promo_WingsOfTheCreator.emo.json',
+        ],
+      ],
+      patches: [
+        {
+          items: [
+            1203945,
+          ],
+          claim: 'Promotional',
+          claimDescription: 'Pre-purchase bonus.',
+          outOfRotation: true,
+          premium: true,
+        },
+        {
+          items: [
+            1195579,
+          ],
+          claim: 'Promotional',
+          claimDescription: 'Pre-purchase bonus.',
+          outOfRotation: true,
+          premium: true,
+        },
+        {
+          items: [
+            1327414,
+          ],
+          claim: 'Promotional',
+          claimDescription: 'Included with the Diablo 4 Deluxe and Ultimate editions.',
+          premium: true,
+        },
+        {
+          items: [
+            1327219,
+          ],
+          claim: 'Promotional',
+          claimDescription: 'Included with the Diablo 4 Deluxe and Ultimate editions.',
+          premium: true,
+        },
+        {
+          items: [
+            1308904,
+          ],
+          claim: 'Promotional',
+          claimDescription: 'Included with the Diablo 4 Ultimate edition.',
+          premium: true,
+        },
+      ],
+      children: [],
+    },
+  ],
+};
+
+export default SOFTWARE;
