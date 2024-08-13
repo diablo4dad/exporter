6b5742db6ac80d94556c688d5c64e03fe7d0b2dd
Sam Edge <guitarfella@gmail.com>
2024-08-14 07:53:35 +0930

    Add challenge boilerplate

---
+++ src/json/collections/challenge/index.ts
@@ -0,0 +1,10 @@
+import { Category, CollectionDescriptor } from '../../index.js';
+
+const CHALLENGE: CollectionDescriptor = {
+  name: 'Challenge',
+  description: 'Challenges',
+  category: Category.CHALLENGE,
+  challengeFile: "json\\base\\meta\\Challenge\\Global.cha.json",
+};
+
+export default CHALLENGE;
