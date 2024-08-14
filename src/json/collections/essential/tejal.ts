ee5a402028e7a7327ea972989063a8a55edce63f
Sam Edge <guitarfella@gmail.com>
2024-08-14 17:28:01 +0930

    Tidy tejals shop collection

--- src/json/collections/essential/tejal.ts
+++ src/json/collections/essential/tejal.ts
@@ -1,11 +1,10 @@
 import { Category, CollectionDescriptor } from '../../index.js';
 
-
 const FIRST_YEAR_ANNIVERSARY: CollectionDescriptor = {
   name: '1st Year Anniversary',
   description: 'Free from Tejal\'s Shop from June 6th, 2024 until June 20th, 2024',
   category: Category.SHOP_ITEMS,
-  claimDescription: '1st Year Anniversary gift free in the cash shop.',
+  claimDescription: 'Free from the cash shop for a limited time.',
   outOfRotation: true,
   items: [
     ['json\\base\\meta\\Item\\mnt_stor045_horse.itm.json'],
@@ -22,8 +21,8 @@ const DARK_GIFTS: CollectionDescriptor = {
   name: 'Dark Gifts',
   description: 'Free from Tejal\'s Shop from December 20th, 2023 until December 26th 2023',
   category: Category.SHOP_ITEMS,
-  claim: "Cash Shop",
-  claimDescription: 'Free in the cash shop for a limited time.',
+  claim: 'Cash Shop',
+  claimDescription: 'Free from the cash shop for a limited time.',
   outOfRotation: true,
   items: [
     ['json\\base\\meta\\Item\\mnt_amor102_horse_stor.itm.json'],
@@ -34,348 +33,71 @@ const DARK_GIFTS: CollectionDescriptor = {
 
 const MOTHERS_FAVOR: CollectionDescriptor = {
   name: 'Mother\'s Favor',
-  description: 'Weapon Cosmetics',
-  category: Category.GENERAL,
+  description: 'Free weapon cosmetics',
+  category: Category.SHOP_ITEMS,
+  claimDescription: 'Occasionally free from the cash shop.',
   items: [
-    [
-      'json\\base\\meta\\Item\\twoHandSword_stor005.itm.json',
-    ],
-    [
-      'json\\base\\meta\\Item\\offHandsSorc_stor009.itm.json',
-    ],
-    [
-      'json\\base\\meta\\Item\\TwoHandCrossbow_stor003.itm.json',
-    ],
-    [
-      'json\\base\\meta\\Item\\Mace_stor007.itm.json',
-    ],
-    [
-      'json\\base\\meta\\Item\\Wand_stor007.itm.json',
-    ],
-    [
-      'json\\base\\meta\\Item\\twoHandDruidStaff_stor008.itm.json',
-    ],
-    [
-      'json\\base\\meta\\Item\\Sword_stor011.itm.json',
-    ],
-    [
-      'json\\base\\meta\\Item\\offHandsNecro_stor006.itm.json',
-    ],
-    [
-      'json\\base\\meta\\Item\\Dagger_stor016.itm.json',
-    ],
-    [
-      'json\\base\\meta\\Item\\twoHandAxe_stor007.itm.json',
-    ],
-    [
-      'json\\base\\meta\\Item\\Sword_stor010.itm.json',
-    ],
+    ['json\\base\\meta\\Item\\twoHandSword_stor005.itm.json'],
+    ['json\\base\\meta\\Item\\offHandsSorc_stor009.itm.json'],
+    ['json\\base\\meta\\Item\\TwoHandCrossbow_stor003.itm.json'],
+    ['json\\base\\meta\\Item\\Mace_stor007.itm.json'],
+    ['json\\base\\meta\\Item\\Wand_stor007.itm.json'],
+    ['json\\base\\meta\\Item\\twoHandDruidStaff_stor008.itm.json'],
+    ['json\\base\\meta\\Item\\Sword_stor011.itm.json'],
+    ['json\\base\\meta\\Item\\offHandsNecro_stor006.itm.json'],
+    ['json\\base\\meta\\Item\\Dagger_stor016.itm.json'],
+    ['json\\base\\meta\\Item\\twoHandAxe_stor007.itm.json'],
+    ['json\\base\\meta\\Item\\Sword_stor010.itm.json'],
   ],
-  patches: [
-    {
-      items: [
-        1238096,
-      ],
-      claim: 'Cash Shop',
-      claimDescription: 'Occasionally redeemable from the shop as a gift.',
-    },
-    {
-      items: [
-        1238174,
-      ],
-      claim: 'Cash Shop',
-      claimDescription: 'Occasionally redeemable from the shop as a gift.',
-    },
-    {
-      items: [
-        1324620,
-      ],
-      claim: 'Cash Shop',
-      claimDescription: 'Occasionally redeemable from the shop as a gift.',
-    },
-    {
-      items: [
-        1238117,
-      ],
-      claim: 'Cash Shop',
-      claimDescription: 'Occasionally redeemable from the shop as a gift.',
-    },
-    {
-      items: [
-        1237413,
-      ],
-      claim: 'Cash Shop',
-      claimDescription: 'Occasionally redeemable from the shop as a gift.',
-    },
-    {
-      items: [
-        1237422,
-      ],
-      claim: 'Cash Shop',
-      claimDescription: 'Occasionally redeemable from the shop as a gift.',
-    },
-    {
-      items: [
-        1238087,
-      ],
-      claim: 'Cash Shop',
-      claimDescription: 'Occasionally redeemable from the shop as a gift.',
-    },
-    {
-      items: [
-        1237303,
-      ],
-      claim: 'Cash Shop',
-      claimDescription: 'Occasionally redeemable from the shop as a gift.',
-    },
-    {
-      items: [
-        1237273,
-      ],
-      claim: 'Cash Shop',
-      claimDescription: 'Occasionally redeemable from the shop as a gift.',
-    },
-    {
-      items: [
-        1278248,
-      ],
-      claim: 'Cash Shop',
-      claimDescription: 'Occasionally redeemable from the shop as a gift.',
-    },
-    {
-      items: [
-        1237309,
-      ],
-      claim: 'Cash Shop',
-      claimDescription: 'Occasionally redeemable from the shop as a gift.',
-    },
-  ],
-  children: [],
 };
 
 const RETURNING_CHAMPION: CollectionDescriptor = {
   name: 'Returning Champion',
-  description: 'Weapon Cosmetics',
-  category: Category.GENERAL,
+  description: 'Free weapon cosmetics',
+  category: Category.SHOP_ITEMS,
+  claimDescription: 'Occasionally free from the cash shop.',
   items: [
-    [
-      'json\\base\\meta\\Item\\Dagger_stor040.itm.json',
-    ],
-    [
-      'json\\base\\meta\\Item\\Sword_stor041.itm.json',
-    ],
-    [
-      'json\\base\\meta\\Item\\Wand_stor019.itm.json',
-    ],
-    [
-      'json\\base\\meta\\Item\\axe_stor018.itm.json',
-    ],
-    [
-      'json\\base\\meta\\Item\\twoHandMace_stor015.itm.json',
-    ],
-    [
-      'json\\base\\meta\\Item\\twoHandDruidStaff_stor025.itm.json',
-    ],
-    [
-      'json\\base\\meta\\Item\\twoHandSword_stor016.itm.json',
-    ],
-    [
-      'json\\base\\meta\\Item\\Mace_stor023.itm.json',
-    ],
-    [
-      'json\\base\\meta\\Item\\Sword_stor047.itm.json',
-    ],
-    [
-      'json\\base\\meta\\Item\\shield_stor013.itm.json',
-    ],
-    [
-      'json\\base\\meta\\Item\\TwoHandCrossbow_stor024.itm.json',
-    ],
-    [
-      'json\\base\\meta\\Item\\offHandsSorc_stor024.itm.json',
-    ],
-    [
-      'json\\base\\meta\\Item\\twoHandAxe_stor014.itm.json',
-    ],
-    [
-      'json\\base\\meta\\Item\\scythe_stor013.itm.json',
-    ],
+    ['json\\base\\meta\\Item\\Dagger_stor040.itm.json'],
+    ['json\\base\\meta\\Item\\Sword_stor041.itm.json'],
+    ['json\\base\\meta\\Item\\Wand_stor019.itm.json'],
+    ['json\\base\\meta\\Item\\axe_stor018.itm.json'],
+    ['json\\base\\meta\\Item\\twoHandMace_stor015.itm.json'],
+    ['json\\base\\meta\\Item\\twoHandDruidStaff_stor025.itm.json'],
+    ['json\\base\\meta\\Item\\twoHandSword_stor016.itm.json'],
+    ['json\\base\\meta\\Item\\Mace_stor023.itm.json'],
+    ['json\\base\\meta\\Item\\Sword_stor047.itm.json'],
+    ['json\\base\\meta\\Item\\shield_stor013.itm.json'],
+    ['json\\base\\meta\\Item\\TwoHandCrossbow_stor024.itm.json'],
+    ['json\\base\\meta\\Item\\offHandsSorc_stor024.itm.json'],
+    ['json\\base\\meta\\Item\\twoHandAxe_stor014.itm.json'],
+    ['json\\base\\meta\\Item\\scythe_stor013.itm.json'],
   ],
-  patches: [
-    {
-      items: [
-        1363030,
-      ],
-      claim: 'Cash Shop',
-      claimDescription: 'Occasionally redeemable from the shop as a gift.',
-    },
-    {
-      items: [
-        1363884,
-      ],
-      claim: 'Cash Shop',
-      claimDescription: 'Occasionally redeemable from the shop as a gift.',
-    },
-    {
-      items: [
-        1365635,
-      ],
-      claim: 'Cash Shop',
-      claimDescription: 'Occasionally redeemable from the shop as a gift.',
-    },
-    {
-      items: [
-        1364185,
-      ],
-      claim: 'Cash Shop',
-      claimDescription: 'Occasionally redeemable from the shop as a gift.',
-    },
-    {
-      items: [
-        1369005,
-      ],
-      claim: 'Cash Shop',
-      claimDescription: 'Occasionally redeemable from the shop as a gift.',
-    },
-    {
-      items: [
-        1596148,
-      ],
-      claim: 'Cash Shop',
-      claimDescription: 'Occasionally redeemable from the shop as a gift.',
-    },
-    {
-      items: [
-        1596412,
-      ],
-      claim: 'Cash Shop',
-      claimDescription: 'Occasionally redeemable from the shop as a gift.',
-    },
-    {
-      items: [
-        1595773,
-      ],
-      claim: 'Cash Shop',
-      claimDescription: 'Occasionally redeemable from the shop as a gift.',
-    },
-    {
-      items: [
-        1581940,
-      ],
-      claim: 'Cash Shop',
-      claimDescription: 'Occasionally redeemable from the shop as a gift.',
-    },
-    {
-      items: [
-        1583356,
-      ],
-      claim: 'Cash Shop',
-      claimDescription: 'Occasionally redeemable from the shop as a gift.',
-    },
-    {
-      items: [
-        1463528,
-      ],
-      claim: 'Cash Shop',
-      claimDescription: 'Occasionally redeemable from the shop as a gift.',
-    },
-    {
-      items: [
-        1463552,
-      ],
-      claim: 'Cash Shop',
-      claimDescription: 'Occasionally redeemable from the shop as a gift.',
-    },
-    {
-      items: [
-        1463536,
-      ],
-      claim: 'Cash Shop',
-      claimDescription: 'Occasionally redeemable from the shop as a gift.',
-    },
-    {
-      items: [
-        1463512,
-      ],
-      claim: 'Cash Shop',
-      claimDescription: 'Occasionally redeemable from the shop as a gift.',
-    },
-  ],
-  children: [],
 };
 
 const THE_HUNGER: CollectionDescriptor = {
   name: 'The Hunger',
-  description: 'Weapon Cosmetics',
-  category: Category.GENERAL,
+  description: 'Free weapon cosmetics',
+  category: Category.SHOP_ITEMS,
+  claimDescription: 'Occasionally free from the cash shop.',
   items: [
-    [
-      'json\\base\\meta\\Item\\twoHandBow_stor006.itm.json',
-    ],
-    [
-      'json\\base\\meta\\Item\\twoHandSorcStaff_stor009.itm.json',
-    ],
-    [
-      'json\\base\\meta\\Item\\twoHandPolearm_stor004.itm.json',
-    ],
-    [
-      'json\\base\\meta\\Item\\offHandsDruid_stor005.itm.json',
-    ],
-    [
-      'json\\base\\meta\\Item\\twoHandScythe_stor006.itm.json',
-    ],
-  ],
-  patches: [
-    {
-      items: [
-        1238959,
-      ],
-      claim: 'Cash Shop',
-      claimDescription: 'Occasionally redeemable from the shop as a gift.',
-    },
-    {
-      items: [
-        1239031,
-      ],
-      claim: 'Cash Shop',
-      claimDescription: 'Occasionally redeemable from the shop as a gift.',
-    },
-    {
-      items: [
-        1238894,
-      ],
-      claim: 'Cash Shop',
-      claimDescription: 'Occasionally redeemable from the shop as a gift.',
-    },
-    {
-      items: [
-        1238907,
-      ],
-      claim: 'Cash Shop',
-      claimDescription: 'Occasionally redeemable from the shop as a gift.',
-    },
-    {
-      items: [
-        1325207,
-      ],
-      claim: 'Cash Shop',
-      claimDescription: 'Occasionally redeemable from the shop as a gift.',
-    },
+    ['json\\base\\meta\\Item\\twoHandBow_stor006.itm.json'],
+    ['json\\base\\meta\\Item\\twoHandSorcStaff_stor009.itm.json'],
+    ['json\\base\\meta\\Item\\twoHandPolearm_stor004.itm.json'],
+    ['json\\base\\meta\\Item\\offHandsDruid_stor005.itm.json'],
+    ['json\\base\\meta\\Item\\twoHandScythe_stor006.itm.json'],
   ],
 };
 
 const TEJAL: CollectionDescriptor = {
   name: 'Tejal\'s Gifts',
-  description: 'Free transmogs occasionally rotated in the shop',
+  description: 'Transmogs acquired from Tejal\'s Shop on a limited basis',
   category: Category.GENERAL,
-  items: [],
-  patches: [],
   children: [
+    FIRST_YEAR_ANNIVERSARY,
+    DARK_GIFTS,
     MOTHERS_FAVOR,
     RETURNING_CHAMPION,
     THE_HUNGER,
-    FIRST_YEAR_ANNIVERSARY,
-    DARK_GIFTS,
   ],
 };
 
