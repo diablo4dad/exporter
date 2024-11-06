d7fadd83e92e237648a20ec6b57f500946e9d831
Sam Edge <guitarfella@gmail.com>
2024-11-06 20:05:15 +1030

    Add commander CLI interface

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import fs from 'fs';
 import path from 'path';
 
+import { Command } from '@commander-js/extra-typings';
+
 import { createAssociationMap, writeSnoRefFile } from './codegen/index.js';
 import { makeDb } from './collection/compiler.js';
 import { PATH_TO_PUBLIC_DIR } from './config.js';
@@ -8,50 +10,68 @@ import { ARTIFACT_NAME, BUILD_DIR } from './constants.js';
 import { readD4Data } from './d4reader/client.js';
 import { copyImages, uploadMissingIcons } from './images/index.js';
 
-const deps = readD4Data();
-const dadDb = makeDb(deps);
+const program = new Command()
+  .name('d4dad-export')
+  .description('CLI for interacting with d4dad tools.')
+  .version('1.0.0');
 
-const doGenSnoIds = true;
-const doWriteDadDb = false;
-const doCopyImages = false;
-const doUpload = false;
-const skipExisting = false;
+program
+  .command('codegen')
+  .description('Generates SNO ID constants.')
+  .action(() => {
+    const deps = readD4Data();
+    const items = createAssociationMap(deps.items, deps.strings, deps.storeProducts);
+    const stream = fs.createWriteStream('debug.ts');
+    writeSnoRefFile(items, stream);
+    stream.close();
+  });
 
-if (doGenSnoIds) {
-  const items = createAssociationMap(deps.items, deps.strings, deps.storeProducts);
-  const stream = fs.createWriteStream('debug.ts');
-  writeSnoRefFile(items, stream);
-  stream.close();
-}
+program
+  .command('build')
+  .description('Generates the d4dad.json.')
+  .action(() => {
+    const deps = readD4Data();
+    const dadDb = makeDb(deps);
 
-if (doWriteDadDb) {
-  if (!fs.existsSync(BUILD_DIR)) {
-    fs.mkdirSync(BUILD_DIR);
-  }
+    if (!fs.existsSync(BUILD_DIR)) {
+      fs.mkdirSync(BUILD_DIR);
+    }
 
-  const dest = path.join(PATH_TO_PUBLIC_DIR, ARTIFACT_NAME);
-  const blob = JSON.stringify(dadDb);
+    const dest = path.join(PATH_TO_PUBLIC_DIR, ARTIFACT_NAME);
+    const blob = JSON.stringify(dadDb);
 
-  fs.writeFileSync(dest, blob);
+    fs.writeFileSync(dest, blob);
 
-  console.log(`Wrote ${dest}.`);
-}
+    console.log(`Wrote ${dest}.`);
+  });
 
-if (doCopyImages) {
-  const iconDest = path.join(PATH_TO_PUBLIC_DIR, 'icons');
-  copyImages(dadDb, iconDest, doUpload, skipExisting)
-    .then(() => {
-      console.log('Copy Images complete.');
+const defaultDest = path.join(PATH_TO_PUBLIC_DIR, 'icons');
 
-      uploadMissingIcons(iconDest, doUpload)
-        .then(() => {
-          console.log('Uploaded missing icons.');
-        })
-        .catch((e) => {
-          console.error('Error uploading icons', e);
-        });
-    })
-    .catch((e) => {
-      console.error('Error', e);
-    });
-}
+program
+  .command('copy-assets')
+  .description('Copies icon assets into the destination folder.')
+  .option('--upload', 'uploads assets to firestore')
+  .option('--skip-existing', 'prevents uploading existing files')
+  .option('--destination <string>', 'directory to copy assets to', defaultDest)
+  .action(async (options) => {
+    const upload = !!options.upload;
+    const skipExisting = !!options.skipExisting;
+    const destination = options.destination;
+
+    const deps = readD4Data();
+    const dadDb = makeDb(deps);
+
+    try {
+      await copyImages(dadDb, destination, upload, skipExisting);
+    } catch (e) {
+      console.error('Error uploading icons.', e);
+    }
+
+    try {
+      await uploadMissingIcons(destination, upload);
+    } catch (e) {
+      console.error('Error uploading missing icons.', e);
+    }
+  });
+
+program.parse();
